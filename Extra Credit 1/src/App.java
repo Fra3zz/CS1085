6dd3257ad90c887a7f76e501167f4af0b9b12f91
Fra3zz <112791937+Fra3zz@users.noreply.github.com>
2025-02-10 13:28:58 -0600

    Removed devision from code per assignment instructions.

--- Extra Credit 1/src/App.java
+++ Extra Credit 1/src/App.java
@@ -2,23 +2,40 @@ public class App {
     public static void main(String[] args) throws Exception {
         
 
+        //IDEAS
+        //Take teh last number, add it to the string
+
+        //Truncate the data by mutiplying the number by a decmal and then making it an int.  Decimal to number.
+
+
         /* User input 5 digit number
            Ex: int num = 53954;      */
 
-        int num = 12345;
+        int num = 59375;
 
         //First print out of original num
         System.out.println("The original number is: " + num);
 
         // Define number variables
         int num1, num2, num3, num4, num5;
+        double numDouble;
 
-        // Modulo minus previouse result to get 1-5 digits
+        // Number isolation from num.
         num1 = num  % 10; // = 4
-        num2 =  (num % 100 - num1) / 10; // = 5
-        num3 =  (num % 1000 - num2) / 100; // = 9
-        num4 =  (num % 10000 - num3) / 1000; // = 3
-        num5 = (num % 100000 - num4) / 10000; // = 5
+
+        //Multiples num by 1e-1 to 1e-4 to make num into a double. Double is truncated and modulo 10 to get isolated nuumber.
+        numDouble = num * 0.1;
+        num2 = (int)numDouble % 10;
+
+        numDouble = num * 0.01;
+        num3 = (int)numDouble % 10;
+
+        numDouble = num * 0.001;
+        num4 = (int)numDouble % 10;
+
+        numDouble = num * 0.0001;
+        num5 = (int)numDouble % 10;
+
 
         //Prints out invers of num with message
         System.out.println("The number in reverse is: " + num1 + num2 + num3 + num4 + num5);
