848c583f137e037cfe98dc6647f8ba6c6b6e24fc
Fra3zz <112791937+Fra3zz@users.noreply.github.com>
2025-04-04 13:10:27 -0500

    Corrected diceRoll method returning 0 as a dice onl has numbers 1-6.

--- Project_1/game.java
+++ Project_1/game.java
@@ -21,8 +21,14 @@ public class game {
     public static String diceRoll() {
         Random rand = new Random();
         //int arry[] = {rand.nextInt(6) + 1, rand.nextInt(6) + 1};
-        return("" + rand.nextInt(6 + 1) + rand.nextInt(6 + 1));
+        return("" + (rand.nextInt(6) + 1) + (rand.nextInt(6) + 1));
     }
+
+    // public static int numFromDice() {
+    //     String numString = diceRoll();
+
+    //     return 0;
+    // }
 
 
 
@@ -40,7 +46,6 @@ public class game {
 
     
     //-----------LOGIC------------
-
         
     }
 }
