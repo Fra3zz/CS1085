38cd89fc4f15e5c69b8ab8aeec754ea173f311fd
Fra3zz <112791937+Fra3zz@users.noreply.github.com>
2025-04-04 11:15:35 -0500

    diceRoll commant.

--- Project_1/game.java
+++ Project_1/game.java
@@ -2,6 +2,7 @@ import java.util.Random;
 
 public class game {
 
+    //Takes in no input and returns a string of two random numbers, concatinated together.
     public static String diceRoll() {
         Random rand = new Random();
         //int arry[] = {rand.nextInt(6) + 1, rand.nextInt(6) + 1};
