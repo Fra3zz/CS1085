9d3e295541012653479e7df3d61ae6b97177c078
Fra3zz <112791937+Fra3zz@users.noreply.github.com>
2025-04-04 11:14:49 -0500

    Added diceRoll method.

--- Project_1/game.java
+++ Project_1/game.java
@@ -1,3 +1,15 @@
+import java.util.Random;
+
 public class game {
-    
+
+    public static String diceRoll() {
+        Random rand = new Random();
+        //int arry[] = {rand.nextInt(6) + 1, rand.nextInt(6) + 1};
+        return("" + rand.nextInt(6 + 1) + rand.nextInt(6 + 1));
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
 }
