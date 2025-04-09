f416f3745687cd833f869cdbd1b1d6af7c704f04
Fra3zz <112791937+Fra3zz@users.noreply.github.com>
2025-04-09 13:16:03 -0500

    Added read method with basic reading capability. Only prints out the file contents in DEBUG.

--- Project_1/game.java
+++ Project_1/game.java
@@ -1,8 +1,11 @@
 import java.util.Random;
 import java.io.PrintWriter;
+import java.nio.Buffer;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class game {
 
@@ -17,22 +20,26 @@ public class game {
         }
     }
 
+    public static void readFile(String filePath, boolean DEBUG){
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while((line = reader.readLine()) != null){
+
+                if(DEBUG){
+                    System.out.printf("DEBUG: %s\n", line); //DEBUG only output
+                }
+            }
+        } catch (IOException e){
+            System.err.printf("\nERROR: %s",e);
+        }
+    }
+
     //Takes no input and returns a string of two random numbers, concatinated together.
     public static String diceRoll() {
         Random rand = new Random();
         //int arry[] = {rand.nextInt(6) + 1, rand.nextInt(6) + 1};
         return("" + (rand.nextInt(6) + 1) + (rand.nextInt(6) + 1));
     }
-
-    // public static int numFromDice() {
-    //     String numString = diceRoll();
-
-    //     return 0;
-    // }
-
-
-
-
 
     public static void main(String[] args) {
 
@@ -45,7 +52,8 @@ public class game {
     boolean DEBUG = true;
 
     
-    //-----------LOGIC------------
-        
+    //-----------METHOD CALLS------------
+
+        readFile(SAVEPATH, DEBUG);
     }
 }
