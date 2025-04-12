5399270e6f8c28d8f43351cdcb40dfac39d068ae
Fra3zz <112791937+Fra3zz@users.noreply.github.com>
2025-04-12 12:03:05 -0500

    Added email validation method.

--- Project_1/game.java
+++ Project_1/game.java
@@ -16,9 +16,12 @@ public class game {
      * @param 
      * @throws IOException
      */
-    public static void writeLineToFile(String content, String filePath){
+    public static void writeLineToFile(String content, String filePath, boolean DEBUG){
          try(PrintWriter writer = new PrintWriter(new File(filePath))){
             writer.println(content);
+            if(DEBUG){
+                System.out.printf("DEBUG: Line was writen to file '%s' with content '%s'.", filePath, content);
+            }
         } catch(IOException e) {
             System.err.printf("ERROR: %s", e.getMessage());
         }
@@ -46,17 +49,78 @@ public class game {
         }
     }
 
+    public static boolean searchUser(String userName, String email, boolean DEBUG) {
+
+    return false;}
+        
+    /**
+     * Evaluates email string for defined formatting of an appropriate email.
+     * @author Fra3zz
+     * @version 1.0.0
+     * @return boolean
+     * @param 
+     * @throws IOException
+     */
+    public static boolean validateEmail(String email, boolean DEBUG) {
+        boolean no_invalid_chars = true;
+        String[] invalidChars = {"[", "]", "\"", "\\", ":", ";", ",", "<", ">", "(", ")", "/"};
+        boolean validStartChar;
+        boolean validEndChar;
+  
+        for(int i = 0; i<invalidChars.length; i++){
+            if(email.contains(invalidChars[i])){
+                no_invalid_chars = false;
+            }
+        }
+        validStartChar = Character.isLetterOrDigit(email.strip().charAt(0));
+        validEndChar = Character.isLetterOrDigit(email.strip().charAt(email.length() - 1));
+
+        if(no_invalid_chars && email.contains("@") && email.contains(".") && !email.isEmpty() && validStartChar && validEndChar){
+            String split_regex = "[@]";
+            String[] first_email_fragments = email.toLowerCase().trim().split(split_regex);
+            if(first_email_fragments.length == 2 && !first_email_fragments[0].isBlank() && !first_email_fragments[1].isBlank()){
+                if(first_email_fragments[0].startsWith(".") || first_email_fragments[0].endsWith(".") || first_email_fragments[1].startsWith(".") || first_email_fragments[0].endsWith(".")){
+                    if(DEBUG){
+                        System.out.printf("DEBUG: First part of email: '%s'\nDEBUG: Second part of email: '%s'\n", first_email_fragments[0], first_email_fragments[1]);
+                        System.out.printf("DEBUG: Full email: '%s@%s'\n", first_email_fragments[0], first_email_fragments[1]);
+                    }
+                    return false;
+                }
+                if(DEBUG){
+                    System.out.printf("DEBUG: First part of email: '%s'\nDEBUG: Second part of email: '%s'\n", first_email_fragments[0], first_email_fragments[1]);
+                    System.out.printf("DEBUG: Full email: '%s@%s'\n", first_email_fragments[0], first_email_fragments[1]);
+                }
+                return true;
+            } else{
+                if(DEBUG){
+                    System.out.printf("DEBUG: First part of email: '%s'\nDEBUG: Second part of email: '%s'\n", first_email_fragments[0], first_email_fragments[1]);
+                    System.out.printf("DEBUG: Full email: '%s@%s'\n", first_email_fragments[0], first_email_fragments[1]);
+                }
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public static void registerUser(String username, String email, boolean DEBUG){
+
+    }
+
     /**
      * Simulates a roll of two 1-6 dice and returns a concatinated string of the resulting numbers.
-     * 
      * @author Fra3zz
      * @version 1.0.0
      * @return String
      * @param 
      */
-    public static String diceRoll() {
+    public static String diceRoll(boolean DEBUG) {
         Random rand = new Random();
-        return("" + (rand.nextInt(6) + 1) + (rand.nextInt(6) + 1));
+        String number = "" + (rand.nextInt(6) + 1) + (rand.nextInt(6) + 1);
+        if(DEBUG){
+            System.out.printf("DEBUG: NUmber generated is %s.",number);
+        }
+        return(number);
     }
 
     public static void main(String[] args) {
@@ -72,6 +136,5 @@ public class game {
     
     //-----------METHOD CALLS------------
 
-        writeLineToFile(SAVEPATH, SAVEPATH);
     }
 }
